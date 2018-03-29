ecc3ca4f14a80a7c9c78188748ca7058c8bb640f
johannes <johannes@192.168.0.80>
2018-03-29 13:57:08 +0200

    Verbesserungen

--- Aufgabe0/Aufgabe0.ts
+++ Aufgabe0/Aufgabe0.ts
@@ -1,5 +1,5 @@
-function Feld() {
-  var eingabe = prompt("Wie lautet Ihr Name?", "");
+function Feld(): void {
+  var eingabe : string = prompt("Wie lautet Ihr Name?", "");
   if (eingabe != null) {
     document.getElementById("HTML").innerHTML =
       "Herzlich Willkommen " + eingabe + "!";
