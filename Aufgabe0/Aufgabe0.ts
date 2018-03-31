2b058a77c1230eb975409e0a6ab1318b6cb3bfab
johannes <johannes@192.168.0.80>
2018-03-31 11:44:29 +0200

    Neuer Versuch

--- Aufgabe0/Aufgabe0.ts
+++
@@ -1,7 +0,0 @@
-function Feld(): void {
-  var eingabe : string = prompt("Wie lautet Ihr Name?", "");
-  if (eingabe != null) {
-    document.getElementById("HTML").innerHTML =
-      "Herzlich Willkommen " + eingabe + "!";
-  }
-}
