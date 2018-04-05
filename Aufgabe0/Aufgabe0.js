edd35de75d3a6d1c16fe4e94cd10b77cc762f3c8
johannes <johannes@johanness-mbp.eduroam.hs-furtwangen.de>
2018-04-05 11:55:41 +0200

    Korrektur Aufgabe 0

---
+++ Aufgabe0/Aufgabe0.js
@@ -0,0 +1,8 @@
+function Feld() {
+    var eingabe = prompt("Wie lautet Ihr Name?", "");
+    if (eingabe != null) {
+        document.getElementById("HTML").innerHTML =
+            "Herzlich Willkommen " + eingabe + "!";
+    }
+}
+//# sourceMappingURL=Aufgabe0.js.map
