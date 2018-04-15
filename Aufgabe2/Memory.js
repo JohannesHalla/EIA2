04f226dbd3eef5fe7d2f3ddc097f5d7d016a5a06
johannes <johannes@192.168.0.80>
2018-04-15 16:56:12 +0200

    Memory

--- Aufgabe2/Memory.js
+++ Aufgabe2/Memory.js
@@ -1,1 +1,103 @@
+/* Aufgabe: 2 - Memory
+Name: Halla, Johannes
+Matrikel:258434
+Datum: 15.04.2018
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde
+nicht kopiert und auch nicht diktiert. */
+var Memory;
+(function (Memory) {
+    let numPlayers;
+    let numPairs;
+    let cardContent = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    let cardPush = [];
+    var numPairsInt;
+    var numPlayerInt;
+    document.addEventListener('DOMContentLoaded', main);
+    //Hauptfunktion Ablauf
+    function main() {
+        Player();
+        creatCardList(numPairsInt);
+        enterName(numPlayerInt);
+        creatCards(numPairsInt);
+    }
+    //Spieleranzahl eingeben
+    function Player() {
+        var numPlayer = prompt("Gewünschte Anzahl der Spieler   min. 1 | max. 4", "");
+        numPlayerInt = parseInt(numPlayer);
+        if (numPlayerInt >= 1 && numPlayerInt <= 4) {
+            return numPlayerInt;
+        }
+        else {
+            alert("Deine Zahl liegt nicht zwischen 1 und 4");
+            Player();
+        }
+    }
+    //Kartenpaare eingeben
+    function Pair() {
+        var numPairs = prompt("Gewünschte Anzahl der Kartenpaare   min. 1 | max. 26");
+        numPairsInt = parseInt(numPairs);
+        if (numPairsInt >= 1 && numPairsInt <= 26) {
+            return numPairsInt;
+        }
+        else {
+            alert("Deine Zahl liegt nicht zwischen 1 und 26");
+            Pair();
+        }
+    }
+    let amount = Pair();
+    //Spielernamen erzeugen
+    function enterName(_numPlayer) {
+        let node = document.getElementById("spielernamen");
+        let childNodeHTML;
+        for (let i = 0; i < _numPlayer; i++) {
+            childNodeHTML = "<div>";
+            childNodeHTML += "<h2>";
+            childNodeHTML += "<p class='namen'>";
+            childNodeHTML += "Player " + i;
+            childNodeHTML += "</p>";
+            childNodeHTML += "</h2>";
+            childNodeHTML += " </div> ";
+            node.innerHTML += childNodeHTML;
+        }
+    }
+    //Inhalt der Karten erzeugen
+    function creatCardList(x) {
+        for (let i = 1; i <= x; i++) {
+            var content = cardContent[0];
+            cardPush.push(content);
+            cardPush.push(content);
+            var remove = cardContent.splice(0, 1);
+        }
+    }
+    //Karten erstellen
+    function creatCards(_numPairs, state = ["hidden", "open", "taken"]) {
+        let node = document.getElementById("spielfeld");
+        let childNodeHTML;
+        let i = 0;
+        for (let i = 0; i < _numPairs * 2; i++) {
+            let min = 0;
+            let max = (cardPush.length);
+            var random = Math.floor(Math.random() * Math.floor(max));
+            childNodeHTML = "<div  class='card" + randomStatus() + "' id='Karte" + i + "'>";
+            childNodeHTML += "<h3>";
+            childNodeHTML += cardPush[random];
+            childNodeHTML += "</h3>";
+            childNodeHTML += " </div> ";
+            node.innerHTML += childNodeHTML;
+        }
+    }
+    //Status vergeben 
+    function randomStatus() {
+        let randomStatus = Math.random();
+        if (randomStatus <= .5) {
+            return "hidden";
+        }
+        else if (randomStatus > .5 && randomStatus <= .95) {
+            return "taken";
+        }
+        else if (randomStatus > .95) {
+            return "open";
+        }
+    }
+})(Memory || (Memory = {}));
 //# sourceMappingURL=Memory.js.map
---
+++ Aufgabe2/Memory.ts
@@ -0,0 +1,156 @@
+/* Aufgabe: 2 - Memory 
+Name: Halla, Johannes 
+Matrikel:258434 
+Datum: 15.04.2018 
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde
+nicht kopiert und auch nicht diktiert. */
+
+
+namespace Memory {
+
+    let numPlayers: number;
+    let numPairs: number;
+    let cardContent: string[] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    let cardPush: string[] = [];
+    var numPairsInt: number;
+    var numPlayerInt: number;
+
+
+    document.addEventListener('DOMContentLoaded', main);
+
+//Hauptfunktion Ablauf
+    
+    function main(): void {
+
+        Player();
+
+        creatCardList(numPairsInt);
+
+        enterName(numPlayerInt);
+
+        creatCards(numPairsInt);
+    }
+
+
+//Spieleranzahl eingeben
+    
+    function Player(): number {
+        var numPlayer: string = prompt("Gewünschte Anzahl der Spieler   min. 1 | max. 4", "");
+        numPlayerInt = parseInt(numPlayer);
+
+        if (numPlayerInt >= 1 && numPlayerInt <= 4) {
+            return numPlayerInt;
+
+        }
+        else {
+            alert("Deine Zahl liegt nicht zwischen 1 und 4");
+            Player();
+        }
+    }
+
+
+
+//Kartenpaare eingeben
+    function Pair(): number {
+        var numPairs: string = prompt("Gewünschte Anzahl der Kartenpaare   min. 1 | max. 26");
+        numPairsInt = parseInt(numPairs);
+
+        if (numPairsInt >= 1 && numPairsInt <= 26) {
+            return numPairsInt;
+
+        }
+        else {
+            alert("Deine Zahl liegt nicht zwischen 1 und 26");
+            Pair();
+        }
+
+    }
+
+
+    let amount: number = Pair();
+
+
+
+//Spielernamen erzeugen
+    function enterName(_numPlayer: number): void {
+        let node: any = document.getElementById("spielernamen");
+        let childNodeHTML: string;
+
+        for (let i: number = 0; i < _numPlayer; i++) {
+
+                     
+            childNodeHTML = "<div>";
+            childNodeHTML += "<h2>";
+            childNodeHTML += "<p class='namen'>";
+            childNodeHTML += "Player " + i;
+            childNodeHTML += "</p>";
+            childNodeHTML += "</h2>";
+            childNodeHTML += " </div> ";
+            node.innerHTML += childNodeHTML;
+        }
+    }
+
+
+
+    
+//Inhalt der Karten erzeugen
+    
+    function creatCardList(x: number): void {
+        for (let i: number = 1; i <= x; i++) {
+            var content: string = cardContent[0];
+            cardPush.push(content);
+            cardPush.push(content);
+
+            var remove = cardContent.splice(0, 1);
+
+        }
+    }
+
+
+ //Karten erstellen
+    
+    function creatCards(_numPairs: number, state: string[] = ["hidden", "open", "taken"]): void {
+        let node: any = document.getElementById("spielfeld");
+        let childNodeHTML: string;
+        let i: number = 0;
+
+
+        for (let i: number = 0; i < _numPairs * 2; i++) {
+            let min: number = 0;
+            let max: number = (cardPush.length);
+
+            var random: number = Math.floor(Math.random() * Math.floor(max));
+
+
+
+            childNodeHTML = "<div  class='card" + randomStatus() + "' id='Karte" + i + "'>";
+            childNodeHTML += "<h3>";
+            childNodeHTML += cardPush[random];
+            childNodeHTML += "</h3>";
+            childNodeHTML += " </div> ";
+            node.innerHTML += childNodeHTML;
+
+
+        }
+        
+        
+    }
+    
+ //Status vergeben 
+    
+  function randomStatus(): string {
+    let randomStatus: number = Math.random();
+    if (randomStatus <= .5) {
+      return "hidden";
+    } else if (randomStatus > .5 && randomStatus <= .95) {
+      return "taken";
+    } else if (randomStatus > .95) {
+      return "open";
+    }
+  
+
+
+
+}
+    
+    }
