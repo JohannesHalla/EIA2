0e63ddcd4831fb8f1a611621edc0177f2c66034f
johannes <johannes@192.168.0.83>
2018-06-10 16:07:49 +0200

    Aufgabe 8

---
+++ Aufgabe8/interface.js
@@ -0,0 +1,5 @@
+var Aufgabe8;
+(function (Aufgabe8) {
+    Aufgabe8.studiHomoAssoc = {};
+})(Aufgabe8 || (Aufgabe8 = {}));
+//# sourceMappingURL=interface.js.map
---
+++ Aufgabe8/interface.ts
@@ -0,0 +1,14 @@
+namespace Aufgabe8 {
+   
+    export interface Studi {
+        name: string;
+        firstname: string;
+        matrikel: number;
+        age: number;
+        gender: boolean;
+        studiengang: string;
+    }
+    export interface Studis {         [matrikel: string]: Studi;
+    }
+     export let studiHomoAssoc: Studis = {};  
+}
