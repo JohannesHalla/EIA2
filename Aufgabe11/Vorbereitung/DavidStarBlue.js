8a41295f6604ddaab46bd243c0bc0aba253252c4
johannes <johannes@192.168.0.80>
2018-07-04 21:21:24 +0200

    AbschlussAuqfgabe Konzept

--- Aufgabe11/Vorbereitung/DavidStarBlue.js
+++ Aufgabe11/Vorbereitung/DavidStarBlue.js
@@ -1,1 +1,14 @@
+var Vorbereitung;
+(function (Vorbereitung) {
+    class DavidStarBlue extends Vorbereitung.DavidStar {
+        constructor(_color) {
+            super(_color);
+        }
+        move() {
+            this.x += 2;
+            this.y -= 1;
+        }
+    }
+    Vorbereitung.DavidStarBlue = DavidStarBlue;
+})(Vorbereitung || (Vorbereitung = {}));
 //# sourceMappingURL=DavidStarBlue.js.map
